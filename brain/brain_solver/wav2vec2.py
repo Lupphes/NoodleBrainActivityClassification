f736019ab9c5b37e999c1f435835f5ea1169978b
DBlankvoort <dick.blankvoort@ru.nl>
2024-02-18 17:27:05 +0100

    feat: got an approach which maybe works?

--- brain/brain_solver/wav2vec2.py
+++ brain/brain_solver/wav2vec2.py
@@ -2,17 +2,25 @@ import torch
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 
 
+# def wav2vec2(spectrograms):
+#         processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+#         model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+#         model.eval()
+#         decoded = {}
+#         for file_name, specs in spectrograms.items():
+#             features = processor(specs, sampling_rate=16000, return_tensors="pt", padding=True)
+#             with torch.no_grad():
+#                 logits = model(features.input_values).logits
+#             predicted_ids = torch.argmax(logits, dim=-1)
+#             decoded[file_name] = processor.batch_decode(predicted_ids)
+#         return decoded
+
 class Wav2Vec2:
 
     def wav2vec2(spectrograms):
         processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-        model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-        model.eval()
         decoded = {}
         for file_name, specs in spectrograms.items():
             features = processor(specs, sampling_rate=16000, return_tensors="pt", padding=True)
-            with torch.no_grad():
-                logits = model(features.input_values).logits
-            predicted_ids = torch.argmax(logits, dim=-1)
-            decoded[file_name] = processor.batch_decode(predicted_ids)
+            decoded[file_name] = features.input_values
         return decoded
