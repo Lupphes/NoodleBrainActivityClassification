88dfed158b46ac29a89a925c3c2d9dfbd58d52bc
DBlankvoort <dahblankvoort@gmail.com>
2024-03-02 13:09:41 +0100

    fix: aaa forgot to change __init__.py

--- brain/brain_solver/__init__.py
+++ brain/brain_solver/__init__.py
@@ -2,7 +2,6 @@ from .helpers import Helpers
 from .filters import Filters, FilterType
 from .trainer import Trainer
 from .eeg_dataset import EEGDataset
-from .eeg_dataset import LatentEEGDataset
 from .config import Config
 from .brain_model import BrainModel
 from .wav2vec2 import Wav2Vec2
