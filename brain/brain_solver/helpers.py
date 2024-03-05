2ac19e55cfb8da0a180642ba6bef040e0fd2d543
DBlankvoort <dahblankvoort@gmail.com>
2024-03-05 18:55:46 +0100

    feat: add switch for modeling both latent and non-latent data

--- brain/brain_solver/helpers.py
+++ brain/brain_solver/helpers.py
@@ -313,7 +313,7 @@ class Helpers:
 
     @staticmethod
     def plot_spectrograms(
-        dataloader, train_data_preprocessed, ROWS=2, COLS=3, BATCHES=2
+        dataloader, train_data_preprocessed, ROWS=2, COLS=3, BATCHES=2, plot_latent=False
     ):
         """
         Plots spectrograms from the dataloader batches along with their corresponding labels and EEG IDs.
@@ -336,21 +336,38 @@ class Helpers:
                         break
                     plt.subplot(ROWS, COLS, index + 1)
                     t = y[index]
-                    img = torch.flip(x[index, :, :, 0], (0,)).T
+
+                    img = torch.flip(x[index, :, :, 0], (0,))
+                    if plot_latent:
+                        img = img.T
+                        
                     mn = img.flatten().min()
                     mx = img.flatten().max()
                     img = (img - mn) / (mx - mn)
-                    plt.plot(img)
 
-                    tars = f"[{t[0]:0.2f}]"
-                    for s in t[1:]:
-                        tars += f", {s:0.2f}"
-                    # Adjust the calculation of `eeg_id` index if necessary
-                    eeg_id_index = i * (ROWS * COLS) + index
-                    eeg = train_data_preprocessed.eeg_id.values[eeg_id_index]
-                    plt.title(f"EEG = {eeg}\nTarget = {tars}", size=12)
-                    plt.ylabel("y", size=14)
-                    plt.xlabel("i", size=14)
+                    if plot_latent:
+                        plt.plot(img)
+                        tars = f"[{t[0]:0.2f}]"
+                        for s in t[1:]:
+                            tars += f", {s:0.2f}"
+                        # Adjust the calculation of `eeg_id` index if necessary
+                        eeg_id_index = i * (ROWS * COLS) + index
+                        eeg = train_data_preprocessed.eeg_id.values[eeg_id_index]
+                        plt.title(f"EEG = {eeg}\nTarget = {tars}", size=12)
+                        plt.ylabel("y", size=14)
+                        plt.xlabel("i", size=14)
+                    else:
+                        plt.imshow(img)
+                        tars = f"[{t[0]:0.2f}]"
+                        for s in t[1:]:
+                            tars += f", {s:0.2f}"
+                        # Adjust the calculation of `eeg_id` index if necessary
+                        eeg_id_index = i * (ROWS * COLS) + index
+                        eeg = train_data_preprocessed.eeg_id.values[eeg_id_index]
+                        plt.title(f"EEG = {eeg}\nTarget = {tars}", size=12)
+                        plt.yticks([])
+                        plt.ylabel("Frequencies (Hz)", size=14)
+                        plt.xlabel("Time (sec)", size=16)
             plt.show()
 
             if i == BATCHES - 1:
