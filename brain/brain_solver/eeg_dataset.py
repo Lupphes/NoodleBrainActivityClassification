e4e3ab85f8d39248f3685f12b4e232e314c0171c
DBlankvoort <dahblankvoort@gmail.com>
2024-03-06 16:41:22 +0100

    fix: dimensionality of X was wrong

--- brain/brain_solver/eeg_dataset.py
+++ brain/brain_solver/eeg_dataset.py
@@ -57,7 +57,7 @@ class EEGDataset(Dataset):
                     r = int((row["min_offset"] + row["max_offset"]) // 4)
 
                 for k in range(4):
-                    X[j, :, :, k] = self.specs[row.spec_id]
+                    X[j, :, :, :, k] = self.specs[row.spec_id]
 
         else:
             X = np.zeros((len(indexes), 128, 256, 8), dtype="float32")
